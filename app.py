c97e99b150bef7c4cedb1fe0f1bfe0414927c7de
wet-dog <50526237+wet-dog@users.noreply.github.com>
2020-12-22 04:38:50 +0000

    Debug fix attempt for PATCH request.

--- app.py
+++ app.py
@@ -27,10 +27,11 @@ def interactions():
                     'content': json['member']['user']['id']
                 }
             })
+            print(players)
         else:
             players.append(json['member']['user']['id'])
             print(players)
-            url = f"https://discord.com/api/v8/webhooks/application.id/{interaction_token[0]}/messages/@original"
+            url = f"https://discord.com/api/v8/webhooks/{MY_APPLICATION_ID}/{interaction_token[0]}/messages/@original"
             headers = {"Authorization": f"Bot {BOT_TOKEN}"}
             json = {"content": " ".join(players)}
             r = requests.patch(url, headers=headers, json=json)
