43f6063552dd7e0c8cc7fc5287e4d1b766a19dfa
wet-dog <50526237+wet-dog@users.noreply.github.com>
2020-12-22 20:19:05 +0000

    Change interactions to Redis.

--- app.py
+++ app.py
@@ -19,18 +19,15 @@ MY_APPLICATION_ID = 789179929979125821
 CLIENT_PUBLIC_KEY = "5c014e0bf7dec5d459505af626f181d3ff246a34a10faa6a9dd6a2e29613888f"
 BOT_TOKEN = os.environ['BOT_TOKEN']
 
-interaction_token = []
-players = []
-
 @app.route('/interactions', methods=['POST'])
 @verify_key_decorator(CLIENT_PUBLIC_KEY)
 def interactions():
     json = request.json
     if json['type'] == InteractionType.APPLICATION_COMMAND:
-        if not players:
+        if not r.exists("interaction_token"):
             print("---------------EMPTY PLAYERS---------------")
-            players.append(json['member']['user']['id'])
-            interaction_token.append(json['token'])
+            r.set("interaction_token", json['token'])
+            r.rpush("players", json['member']['user']['id'])
             return jsonify({
                 'type': InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
                 'data': {
@@ -40,12 +37,13 @@ def interactions():
             # print(players)
         else:
             print("************PLAYERS********************")
-            players.append(json['member']['user']['id'])
+            r.rpush("players", json['member']['user']['id'])
             # print(players)
+            interaction_token = r.get("interaction_token")
             url = f"https://discord.com/api/v8/webhooks/{MY_APPLICATION_ID}/{interaction_token[0]}/messages/@original"
             headers = {"Authorization": f"Bot {BOT_TOKEN}"}
-            json = {"content": " ".join(players)}
-            r = requests.patch(url, headers=headers, json=json)
+            json = {"content": " ".join(r.get("players"))}
+            req = requests.patch(url, headers=headers, json=json)
             # print(r)
             return jsonify({
                 'type': InteractionResponseType.ACKNOWLEDGE
