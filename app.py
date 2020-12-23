90eaa0f25aee4dfd71dc88df9be7d270d987cc3f
wet-dog <50526237+wet-dog@users.noreply.github.com>
2020-12-23 19:52:42 +0000

    Fix embed json.

--- app.py
+++ app.py
@@ -49,8 +49,9 @@ def interactions():
             content = "\n".join([f"{k}: {v}" for k, v in content.items()])
             embed = discord.Embed(title="Lobby")
             embed.add_field(name="Players", value=content)
-            embed = json.dumps([embed.to_dict()])
-            data = {"content": "", "embeds": embed}
+            embed = [embed.to_dict()]
+            data = {"embeds": embed}
+            data = json.dumps(data)
             requests.patch(url, headers=headers, json=data)
             
             return jsonify({
